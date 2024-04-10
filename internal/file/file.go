c97839438c1118d53d0c0b20ccfa4ac59904d941
Murkadymov <153239939+Murkadymov@users.noreply.github.com>
2024-04-10 04:08:17 +0300

    update

--- internal/file/file.go
+++ internal/file/file.go
@@ -1,6 +1,26 @@
 package file
 
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
+
 type File struct {
+	ID   uuid.UUID
 	Name string
 	Data []byte
+}
+
+func NewFile(filename string, blob []byte) (*File, error) {
+
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return nil, fmt.Errorf("Got error generating uuid: %w", err)
+	}
+
+	return &File{
+		ID:   id,
+		Name: filename,
+		Data: blob,
+	}, nil
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,8 +1,20 @@
 package storage
 
+import "modstorage/internal/file"
+
 type Storage struct {
 }
 
 func NewStorage() *Storage {
 	return &Storage{}
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
+	finalfile, err := file.NewFile(filename, blob)
+	if err != nil {
+		return nil, err
+	}
+
+	return finalfile, nil
+
 }
